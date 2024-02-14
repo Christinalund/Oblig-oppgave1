644943fcf809f275c7f04a1e790d73b2bfed4a20
Christina <chlund84@gmail.com>
2024-02-14 13:29:18 +0100

    First html edit

---
+++ src/main/java/com/example/oblig_oppgave1/ObligOppgave1Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig_oppgave1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligOppgave1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObligOppgave1Application.class, args);
+    }
+
+}
